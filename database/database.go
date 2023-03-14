8388ab747cfbead50110319cd2d611cc2ada3b5c
Fuki Hayashi <knightf884@yahoo.co.jp>
2023-03-15 02:16:05 +0900

    コメント記入

--- database/database.go
+++ database/database.go
@@ -13,14 +13,19 @@ var Db *gorm.DB
 var err error
 
 func SetupDb() {
+	// .envで読み込んだ接続情報
 	dsn := os.Getenv("DATABASE_URL")
 
+	// DBへ接続
 	Db, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		log.Fatal("FAILED TO CONNECT TO DB")
 	} else {
 		log.Println("CONNECTED TO DB")
 	}
+
+	// オートマイグレーション
 	Db.AutoMigrate(&model.Line{}, &model.Element{}, &model.Relation{}, &model.Coordinate{}, &model.OperationalPoint{})
+	// 初期データ投入
 	DbSeed()
 }

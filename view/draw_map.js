754e05f2cdcf97f2190334c2a959c6ddede30abf
Fuki Hayashi <knightf884@yahoo.co.jp>
2023-03-15 02:18:30 +0900

    ビューに矢印を引く機能追加

--- view/draw_map.js
+++ view/draw_map.js
@@ -47,6 +47,45 @@ function DrawLine(line){
                     popup.setLatLng(e.latlng).setContent(busstop.name).openOn(map);
                 })
             }
+            return res
+        }).then(res => {
+            // 矢印を引く
+            for(let r of res.relations){
+                var elementA=[], elementB=[]
+                var arrow = false
+                for(let e of res.elements){
+                    // 矢印の起点
+                    if(r.elementA == e.id){
+                        if(e.coordinates.length>1){
+                            elementA = getCoordinate(e.coordinates[e.coordinates.length-2])
+                        }else if(e.coordinates.length==1){
+                            elementA = getCoordinate(e.coordinates[0])
+                        }else{
+                            console.error("coordinate not found")
+                        }
+                    }
+                    // 矢印の終点
+                    if(r.elementB == e.id){
+                        if(e.coordinates.length>1){
+                            elementB = getCoordinate(e.coordinates[1])
+                        }else if(e.coordinates.length==1){
+                            elementB = getCoordinate(e.coordinates[0])
+                            arrow = true
+                        }else{
+                            console.error("coordinate not found")
+                        }
+                    }
+                }
+                if(arrow) {
+                    var polyline = L.polyline([[elementA[1],elementA[0]],[elementB[1],elementB[0]]]).addTo(map);
+                    var decorator = L.polylineDecorator(polyline, {
+                    patterns: [
+                        {offset: '100%', repeat: 0, symbol: L.Symbol.arrowHead({pixelSize: 5, polygon: true, pathOptions: {stroke: true}})}
+                        ]
+                    }).addTo(map);
+                }
+            }
+            
         })
         .catch(error => {
             console.error(error);
